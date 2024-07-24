d2291fe454f79514ecc6823f68d5a9ffdcf3cd8f
utpalb <utpalb@altair.com>
2024-07-24 17:36:29 +0530

    LIndent formatter added

---
+++ formatters/L-indent.js
@@ -0,0 +1,45 @@
+export class LIndentFormatter {
+    static _depth = 0;
+    static repeatValue = 4;
+    static indentChar = ' ';
+
+    /**
+     * Returns the formatted string
+     * @param {Object} item
+     * @param {Object} funcMap
+     * @returns string 
+     */
+    static _format(item, funcMap) {
+        let formatStr = `${item.name}\n`;
+
+        // If item has no children, nothing to do
+        if (!item.children || !item.children.length) {
+            return formatStr;
+        }
+
+        // Increase the depth value before entering this loop
+        ++this._depth;
+        
+        const indentStr = this.indentChar.repeat((this._depth - 1)*this.repeatValue);
+        for (let id of item.children) {
+            const child = funcMap[id];
+            formatStr += `${indentStr}|_  ${this._format(child, funcMap)}`;
+        }
+
+        // Decrement it back now that we're out
+        --this._depth;
+
+        return formatStr;
+    }
+
+    /**
+     * 
+     * @param {Object} funcMap 
+     * @param {HTMLElement} parentElement 
+     */
+    static format(funcMap, parentElement) {
+        const root = funcMap[0];
+        const text = LIndentFormatter._format(root, funcMap);
+        parentElement.innerText = text;
+    }
+}
--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 import { PlainIndentFormatter } from "./formatters/plain-indent.js";
 import { VSStackParser } from "./parsers/vs-parser.js";
 
+async function copyToClipboardAsync(text) {
+    try {
+        await navigator.clipboard.writeText(text);
+    } catch (err) {
+        console.error('Error copying text:', err);
+    }
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     // Add on paste listener
@@ -9,10 +16,16 @@ window.addEventListener('DOMContentLoaded', () => {
         const text = e.clipboardData.getData('text/plain');
         const funcMap = VSStackParser.parse(text);
 
+        if (!funcMap)
+            return;
+
         console.log(funcMap);
 
         // get the output element
         const output = document.getElementById('output');
         PlainIndentFormatter.format(funcMap, output);
+
+        // Copy the content to clipboard
+        copyToClipboardAsync(output.innerText);
     })
 });
